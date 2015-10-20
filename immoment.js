171ed0c648a4519fa2a53231058c3b10a61a48de
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-10-20 11:39:02 +0200

    Add browser version

---
+++ immoment.js
@@ -0,0 +1,36 @@
+/*
+immoment.js
+Copyright (c) 2015 Rafał Pocztarski
+Released under MIT license
+https://github.com/rsp/node-immoment
+*/
+
+window.immoment = (function (moment) {
+
+function Immoment (mo) {
+    this._mc = function () {
+        return mo.clone();
+    };
+}
+
+Immoment.prorotype = {};
+
+var mo = moment();
+
+for (var m in mo) {
+    Immoment.prototype[m] = (function (m) {
+        return function () {
+            var mc = this._mc();
+            return mc[m].apply(mc, arguments);
+        };
+    }(m));
+}
+
+function immoment () {
+    var mo = moment.apply(this, arguments);
+    return new Immoment(mo);
+}
+
+return immoment;
+
+}(moment));
